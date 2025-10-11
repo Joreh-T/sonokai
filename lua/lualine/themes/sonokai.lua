00a1e624cb4d0995498f1d5bcc15e2f18fcf0f6c
TaoQh <taoqiuhao@inhand.com.cn>
2025-10-11 18:04:17 +0800

    fix(colors): update foreground colors for better contrast in lualine theme

--- lua/lualine/themes/sonokai.lua
+++ lua/lualine/themes/sonokai.lua
@@ -17,36 +17,65 @@ return {
   normal = {
     a = {bg = palette.green[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
   },
   insert = {
     a = {bg = palette.orange[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
   },
   visual = {
     a = {bg = palette.bg_red[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
+
   },
   replace = {
     a = {bg = palette.bg_blue[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
   },
   command = {
     a = {bg = palette.yellow[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg4[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
   },
   terminal = {
     a = {bg = palette.purple[1], fg = palette.bg0[1], gui = 'bold'},
     b = {bg = palette.bg3[1], fg = palette.fg[1]},
-    c = {bg = palette.bg1[1], fg = palette.fg[1]}
+    c = {bg = palette.bg1[1], fg = palette.grey[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey[1]}
+    -- z = 
   },
   inactive = {
-    a = {bg = palette.bg1[1], fg = palette.grey[1]},
-    b = {bg = palette.bg1[1], fg = palette.grey[1]},
-    c = {bg = palette.bg1[1], fg = palette.grey[1]}
+    a = {bg = palette.bg1[1], fg = palette.grey_dim[1]},
+    b = {bg = palette.bg1[1], fg = palette.grey_dim[1]},
+    c = {bg = palette.bg1[1], fg = palette.grey_dim[1]},
+
+    -- x = 
+    y = {bg = palette.bg4[1], fg = palette.grey_dim[1]}
+    -- z = 
   }
 }
